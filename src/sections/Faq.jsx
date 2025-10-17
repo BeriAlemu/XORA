fdd7517cc42e059dcd4e796379863b0c87240ac3
Beri Alemu <berialemu8@gmail.com>
2025-10-17 23:24:26 +0330

    fixed location problem

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,6 +1,6 @@
 import { Element } from "react-scroll";
 import { faq } from "../constants";
-import FaqItem from "../components/Faqitem";
+import FaqItem from "../components/FaqItem";
 
 function Faq (){
     const halfLength = Math.floor(faq.length/2)
